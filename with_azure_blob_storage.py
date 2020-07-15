5b2a7f56f7d83ffc64b38936ccc13c8fd0d29911
yuu-eguci <yuu.eguci@gmail.com>
2020-07-15 13:55:58 +0900

    ✨ with_azure_blob_storage.py base

---
+++ with_azure_blob_storage.py
@@ -0,0 +1,19 @@
+"""Cognitive Services trial with Azure Blob Storage
+
+このスクリプトの目標。
+
+- trial.py で作っているような画像を Azure Blob Storage の画像をもとに作る。
+
+"""
+
+# BlobServiceClient を作成します。
+
+# ContainerClient を作成します。
+
+# BlobClient を作成します。
+
+# 画像を DL します。
+
+# ここでなんちゃらかんちゃらして画像を mat 化します。
+
+# mat 化しちゃえば結合とかやりたい放題です。
