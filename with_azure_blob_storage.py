6a45eeb627d6b1d311f99a497e1eab945502b33d
yuu-eguci <yuu.eguci@gmail.com>
2020-07-17 23:58:21 +0900

    Update with_azure_blob_storage.py

--- with_azure_blob_storage.py
+++ with_azure_blob_storage.py
@@ -23,11 +23,15 @@ blob_service_client = BlobServiceClient.from_connection_string(
     CONNECTION_STRING)
 
 # ContainerClient を作成します。
-container_client = blob_service_client.get_container_client(
-    CONTAINER_NAME)
+# container_client = blob_service_client.get_container_client(
+#     CONTAINER_NAME)
 
 # BlobClient を作成します。
-blob_client = container_client.get_blob_client('100x100-dog.png')
+# blob_client = container_client.get_blob_client('100x100-dog.png')
+
+# BlobClient は BlobServiceClient から直接、 ContainerClient をすっ飛ばして作ることも可能。
+blob_client = blob_service_client.get_blob_client(
+    container=CONTAINER_NAME, blob='100x100-dog.png')
 
 # 画像を DL します。
 downloaded_bytes = blob_client.download_blob().readall()
